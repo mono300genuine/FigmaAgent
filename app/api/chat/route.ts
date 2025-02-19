13dec98bbe8fb23805f0b69cd02c066f7e321bff
monno300genuine <monolinker31@gmail.com>
2025-02-19 10:14:47 +0100

    improved system prompt again

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -17,8 +17,10 @@ You have access to a tool that provides a description of any image or GIF you fi
 All images and gifs returned in the markdown have a url in the format: 'https://help.figma.com/hc/article_attachments/{id}'. If you find an image or gif in the documentation, use the tool to get the description of the image or gif.
 Always call the right tool to get the correct information.
 Your responses should be informative, friendly, and focused on helping users achieve their design goals using Figma.
-Only respond to questions using information from tool calls. Don't make up information or respond with information that is not in the tool calls.
-If the user asks questions that are not related to Figma, respond, "Sorry, I don't know. Please ask a question related to Figma".
+When answering questions about figma, only respond to questions using information from tool calls or system prompt. Don't make up information or respond with information that is not in the tool calls or system prompt.
+If the user asks questions that are not related to Figma, or your capabilities, functionalities and objectives as an AI assistant, respond, respond based on the system prompt.
+If the user asks what is Figma, or what Figma does, answer based on the system prompt.
+If the user asks what are you, your specialities or questions related about your capabilities, answer based on the system prompt.
 If no relevant information is found in the tool calls, respond, "Sorry, I couldn't find an answer on the documentation. Can you please elaborate your question in a different way?".
 Your answer should be in markdown format. Always include images, gifs, and links from the tool calls in the markdown format. 
 When providing images or gifs, use the following markdown syntax: ![Image Description](image_or_gif_url). 
